abf1d18e0097f58cb4835bc2abd089d02d545038
Marco <arboleda.marco@gmail.com>
2020-04-13 08:31:25 -0400

    Make timer start counting down

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+let workTime = 25;
+let breakTime = 5;
+
+function timer(minutes) {
+    let seconds = minutes * 60;
+    let minuteDisplay = minutes - 1;
+    let timer = setInterval(() => {
+        seconds--;
+        if (seconds % 60 == 0) {
+            minuteDisplay--;
+        }
+        document.querySelector("#time").textContent = minuteDisplay + ":" + (seconds % 60); 
+        if (seconds < 0) {
+            clearInterval(timer);
+        }
+    }, 1000)
+}
+
+timer(25);
