385df676f7e6b0e7b8378f5a5bbe74797abe80d1
Platyphyllum <arboleda.marco@gmail.com>
2020-04-18 18:00:00 -0400

    Implement Resume functionality when paused

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,32 @@
 let workTime = 25;
 let breakTime = 5;
 const stopButton = document.querySelector("#stop-button");
+const startButton = document.querySelector("#start-button");
 const timeDisplay = document.querySelector("#time");
 let isBreakTime = false;
-let pause = false;
+let isPaused = false;
 let timer;
 let currentMinutes;
 let currentSeconds;
 
-function startTimer(minutes) {
-    let seconds = minutes * 60;
-    let minuteDisplay = minutes - 1;
+function startTimer(minutes, seconds) {
+    let totalSeconds = minutes * 60 + seconds;
+    let minutesDisplay = minutes;
+
+    if (minutes != 0) {
+        minuteDisplay = minutes - 1;
+    }
     
     timer = setInterval(() => {
-        seconds--;
+        totalSeconds--;
 
-        if ((seconds % 60 == 0) && seconds != 0) {
+        if ((totalSeconds % 60 == 0) && totalSeconds != 0) {
             minuteDisplay--;
         }
 
-        timeDisplay.textContent = minuteDisplay + ":" + (seconds % 60); 
+        timeDisplay.textContent = minuteDisplay + ":" + (totalSeconds % 60); 
 
-        if (seconds <= 0) {
+        if (totalSeconds <= 0) {
             clearInterval(timer);
 
             if (!isBreakTime) {
@@ -42,6 +47,14 @@ stopButton.addEventListener("click", function(e) {
     let minutesAndSeconds = timeDisplay.textContent.split(":");
     currentMinutes = minutesAndSeconds[0];
     currentSeconds = minutesAndSeconds[1];
+    isPaused = true;
 });
 
-startTimer(1);
+startButton.addEventListener("click", function(e) {
+    if (isPaused) {
+        isPaused = false;
+        startTimer(currentMinutes, currentSeconds);
+    }
+})
+
+startTimer(1, 0);
