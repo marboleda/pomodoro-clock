92477a7175d609bc5c36f75706103f6c2224b72c
Platyphyllum <arboleda.marco@gmail.com>
2020-04-18 16:15:29 -0400

    Implement Stop button functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,30 @@
 let workTime = 25;
 let breakTime = 5;
-
+const stopButton = document.querySelector("#stop-button");
+const timeDisplay = document.querySelector("#time");
 let isBreakTime = false;
 let pause = false;
+let timer;
+let currentMinutes;
+let currentSeconds;
 
 function startTimer(minutes) {
     let seconds = minutes * 60;
     let minuteDisplay = minutes - 1;
-    let timer = setInterval(() => {
+    
+    timer = setInterval(() => {
         seconds--;
 
         if ((seconds % 60 == 0) && seconds != 0) {
             minuteDisplay--;
         }
-        
-        document.querySelector("#time").textContent = minuteDisplay + ":" + (seconds % 60); 
+
+        timeDisplay.textContent = minuteDisplay + ":" + (seconds % 60); 
 
         if (seconds <= 0) {
             clearInterval(timer);
 
             if (!isBreakTime) {
-                console.log("You get in here");
                 isBreakTime = true;
                 startTimer(breakTime);
             }
@@ -32,5 +36,12 @@ function startTimer(minutes) {
 
     }, 1000)
 }
+
+stopButton.addEventListener("click", function(e) {
+    clearInterval(timer);
+    let minutesAndSeconds = timeDisplay.textContent.split(":");
+    currentMinutes = minutesAndSeconds[0];
+    currentSeconds = minutesAndSeconds[1];
+});
 
 startTimer(1);
